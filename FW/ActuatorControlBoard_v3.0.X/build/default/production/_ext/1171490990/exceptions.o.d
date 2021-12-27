8880f03f3ec1d02493eee66ceeedc31fcac732bc
milomir98 <milomir.spajic@gmail.com>
2021-12-27 21:39:55 +0100

    Ubacen fw

---
+++ FW/ActuatorControlBoard_v3.0.X/build/default/production/_ext/1171490990/exceptions.o.d
@@ -0,0 +1,44 @@
+build/default/production/_ext/1171490990/exceptions.o: \
+ ../src/config/default/exceptions.c ../src/config/default/device.h \
+ ../src/config/default/toolchain_specifics.h \
+ ../src/config/default/definitions.h \
+ ../src/config/default/peripheral/uart/plib_uart3.h \
+ ../src/config/default/device.h \
+ ../src/config/default/peripheral/uart/plib_uart_common.h \
+ ../src/config/default/peripheral/can/plib_can4.h \
+ ../src/config/default/peripheral/can/plib_can_common.h \
+ ../src/config/default/peripheral/clk/plib_clk.h \
+ ../src/config/default/peripheral/gpio/plib_gpio.h \
+ ../src/config/default/peripheral/evic/plib_evic.h \
+ ../src/config/default/peripheral/uart/plib_uart6.h ../src/pumps.h \
+ ../src/ax12.h ../src/can.h
+
+../src/config/default/device.h:
+
+../src/config/default/toolchain_specifics.h:
+
+../src/config/default/definitions.h:
+
+../src/config/default/peripheral/uart/plib_uart3.h:
+
+../src/config/default/device.h:
+
+../src/config/default/peripheral/uart/plib_uart_common.h:
+
+../src/config/default/peripheral/can/plib_can4.h:
+
+../src/config/default/peripheral/can/plib_can_common.h:
+
+../src/config/default/peripheral/clk/plib_clk.h:
+
+../src/config/default/peripheral/gpio/plib_gpio.h:
+
+../src/config/default/peripheral/evic/plib_evic.h:
+
+../src/config/default/peripheral/uart/plib_uart6.h:
+
+../src/pumps.h:
+
+../src/ax12.h:
+
+../src/can.h:
