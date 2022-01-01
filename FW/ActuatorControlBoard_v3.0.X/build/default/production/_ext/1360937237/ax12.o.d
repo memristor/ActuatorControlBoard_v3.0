b6e01c71b127d12fd41634d9a324149451455b6e
milomir98 <milomir.spajic@gmail.com>
2022-01-01 23:16:55 +0100

    folder naming change

---
+++ FW/ActuatorControlBoard_v3.0.X/build/default/production/_ext/1360937237/ax12.o.d
@@ -0,0 +1,47 @@
+build/default/production/_ext/1360937237/ax12.o: ../src/ax12.c \
+ ../src/config/default/definitions.h \
+ ../src/config/default/peripheral/uart/plib_uart3.h \
+ ../src/config/default/device.h \
+ ../src/config/default/toolchain_specifics.h \
+ ../src/config/default/peripheral/uart/plib_uart_common.h \
+ ../src/config/default/peripheral/coretimer/plib_coretimer.h \
+ ../src/config/default/peripheral/can/plib_can4.h \
+ ../src/config/default/peripheral/can/plib_can_common.h \
+ ../src/config/default/peripheral/clk/plib_clk.h \
+ ../src/config/default/peripheral/gpio/plib_gpio.h \
+ ../src/config/default/peripheral/evic/plib_evic.h \
+ ../src/config/default/peripheral/uart/plib_uart6.h ../src/pumps.h \
+ ../src/ax12.h ../src/can.h \
+ ../src/config/default/peripheral/uart/plib_uart3.h
+
+../src/config/default/definitions.h:
+
+../src/config/default/peripheral/uart/plib_uart3.h:
+
+../src/config/default/device.h:
+
+../src/config/default/toolchain_specifics.h:
+
+../src/config/default/peripheral/uart/plib_uart_common.h:
+
+../src/config/default/peripheral/coretimer/plib_coretimer.h:
+
+../src/config/default/peripheral/can/plib_can4.h:
+
+../src/config/default/peripheral/can/plib_can_common.h:
+
+../src/config/default/peripheral/clk/plib_clk.h:
+
+../src/config/default/peripheral/gpio/plib_gpio.h:
+
+../src/config/default/peripheral/evic/plib_evic.h:
+
+../src/config/default/peripheral/uart/plib_uart6.h:
+
+../src/pumps.h:
+
+../src/ax12.h:
+
+../src/can.h:
+
+../src/config/default/peripheral/uart/plib_uart3.h:
